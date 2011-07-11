a611af229aa7f272619991474261a5ba4c8f7641
Shawn <syst3mfailur3@gmail.com>
2011-07-11 15:42:02 -0400

    Only actions that are being used are included in the snp request. Theres probably a better way (or a few) to do this, but it works for now.

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import pySNP
 
 sig = "application/python_test"
-print sig
 title = "Python Test"
 icon = "!system-info"
 text = "Hello world"
@@ -9,8 +8,7 @@ timeout = "10"
 uid = ""
 password = ""
 
-pySNP.snRegister(sig, title, icon, password)
-pySNP.snNotify(sig, title, text, icon, timeout, uid, password)
-
+pySNP.snRegister(sig, title)
+pySNP.snNotify(sig, title, text)
 pySNP.snVersion()
-pySNP.snUnregister(sig, password)
+pySNP.snUnregister(sig)
--- pySNP.py
+++ pySNP.py
@@ -1,35 +1,73 @@
+import sys
 import socket
 
 host = "127.0.0.1"
 port = 9887
-print "Address:", host, ":", port
 
 def request(action):
+    action = action + "\r\n"
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((host, port))
     sock.send(action)
-    print sock.recv(1024)
+    print sock.recv(1024), action
+    #print action
     sock.close()
 
-def snRegister (sig, title, icon, password):
+def snRegister (sig, title, icon="", password=""):
     print "Snarl: Register"
-    action = ("snp://reg?app-sig=" + sig + "&title=" + title + "&icon=" +
-              icon + "&password=" + password + "\r\n")
+    action = "snp://register?"
+    if sig != "" :
+        action = action + "app-sig=" + sig
+    if title != "":
+        action = action + "&title=" + title
+    if icon != "":
+        action = action + "&icon=" + icon
+    if password != "":
+        action = action + "&password=" + password
+    request(action)
+
+def snNotify (sig, title, text, icon="", timeout="10", uid="", password=""):
+    print "Snarl: Notify"
+    action = "snp://notify?"
+    if sig != "" :
+        action = action + "app-sig=" + sig
+    if title != "":
+        action = action + "&title=" + title
+    if text != "":
+        action = action + "&text=" + text
+    if icon != "":
+        action = action + "&icon=" + icon
+    if timeout == "" or timeout != "":
+        action = action + "&timeout=" + timeout
+    if uid != "":
+        action = action + "&uid=" + uid
+    if password != "":
+        action = action + "&password=" + password
+        
+##    action = ("snp://notify?app-sig=" + sig + "&title=" + title + "&text=" +
+##              text + "&icon=" + icon + "&timeout=" + timeout + "&uid=" + uid +
+##              "&password=" + password)
     request(action)
     
+'''
 def snNotify (sig, title, text, icon, timeout, uid, password):
     print "Snarl: Notify"
     action = ("snp://notify?app-sig=" + sig + "&title=" + title + "&text=" +
               text + "&icon=" + icon + "&timeout=" + timeout + "&uid=" + uid +
-              "&password=" + password + "\r\n")
+              "&password=" + password)
     request(action)
+'''
 
 def snVersion():
     print "Snarl: Version"
-    action = "snp://version" + "\r\n"
+    action = "snp://version"
     request(action)
 
-def snUnregister(sig, password):
-    print "Snarl: Unregister"           
-    action = "snp://unreg?app-sig=" + sig + "&password=" + password + "\r\n"
+def snUnregister(sig, password=""):
+    print "Snarl: Unregister"
+    action = "snp://unregister?"
+    if sig != "" :
+        action = action + "app-sig=" + sig
+    if password != "":
+        action = action + "&password=" + password
     request(action)
