6fef649211382b8cc00079c6dd67bde3b5bfa7c7
Shawn <syst3mfailur3@gmail.com>
2011-07-12 20:45:00 -0400

    if required action missing, show error and send no action.

--- example.py
+++ example.py
@@ -1,16 +1,18 @@
 import pySNP    
 
-sig = "sig"
-token = ""
-title = "title"
-text = "text"
-icon = ""
-timeout = ""
-priority = ""
-uid = ""
-password = ""
+sig = 'SIG'
+token = ''
+title = 'TITLE'
+text = 'TEXT'
+icon = ''
+timeout = ''
+priority = ''
+uid = ''
+password = ''
 
 pySNP.snRegister(sig, token, title, icon, password)
 pySNP.snNotify(sig, token, title, text, icon, timeout, priority, uid, password)
 pySNP.snVersion()
 pySNP.snUnregister(sig, token, password)
+
+x = raw_input('Hit ANY key to exit')
--- pySNP.py
+++ pySNP.py
@@ -1,10 +1,10 @@
 import socket
 
-ip = "127.0.0.1"
+ip = '127.0.0.1'
 port = 9887
 
 def request(snp):
-    snp = snp + "\r\n"
+    snp = snp  + '\r\n'
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((ip, port))
     sock.send(snp)
@@ -12,44 +12,46 @@ def request(snp):
     sock.close()
 
 def action(snp, required, data, value):
+    err = False
+    q = ''
     for r, d, v in zip(required, data, value):
-        if r != "" and v != "":
-            q = d + v
-            snp = snp + q
-        elif r != "" and v == "":
-            print "Snarl Error: '%s' is missing." % d
-            #break
-        elif v != "":
-            q = d + v
-            snp = snp + q
+        if r != '' and v != '':
+            q = q + d + v
+        elif r != '' and v == '':
+            print 'Error: "%s" is missing.' % d
+            err = True
+        elif v != '':
+            q = q + d + v
+    if err == False:
+        snp = snp + q
     request(snp)
 
 def snRegister(sig, token, title, icon, password):
-    print "Snarl: Register"
-    snp = "snp://register?"
-    required = ["app-sig=", "",  "&title=", "", ""]
-    data = ["app-sig=", "&token=", "&title=", "&icon=", "&password="]
+    print 'Snarl: Register'
+    snp = 'snp://register'
+    required = ['?app-sig=', '',  '&title=', '', '']
+    data = ['?app-sig=', '&token=', '&title=', '&icon=', '&password=']
     value = [sig, token, title, icon, password]
     action(snp, required, data, value)
 
 def snNotify(sig, token, title, text, icon, timeout, priority, uid, password):
-    print "Snarl: Notify"
-    snp = "snp://notify?"
-    required = ["app-sig=", "",  "&title=", "&text=", "", "", "", "", ""]
-    data = ["app-sig=", "&token=","&title=", "&text=", "&icon=", "&timeout=",
-            "&priority=", "&uid=", "&password="]
+    print 'Snarl: Notify'
+    snp = 'snp://notify'
+    required = ['?app-sig=', '',  '&title=', '&text=', '', '', '', '', '']
+    data = ['?app-sig=', '&token=','&title=', '&text=', '&icon=', '&timeout=',
+            '&priority=', '&uid=', '&password=']
     value = [sig, token, title, text, icon, timeout, priority, uid, password]
     action(snp, required, data, value)
 
 def snVersion():
-    print "Snarl: Version"
-    snp = "snp://version"
+    print 'Snarl: Version'
+    snp = 'snp://version'
     request(snp)
  
 def snUnregister(sig, token, password):
-    print "Snarl: Unregister"
-    snp = "snp://unregister?"
-    required = ["app-sig=", "", ""]
-    data = ["app-sig=", "&token=", "&password="]
+    print 'Snarl: Unregister'
+    snp = 'snp://unregister'
+    required = ['?app-sig=', '', '']
+    data = ['?app-sig=', '&token=', '&password=']
     value = [sig, token, password]
     action(snp, required, data, value)
