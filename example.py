cb9ea97f5750c49ad7e533380bef79b68c08e4d5
Shawn <syst3mfailur3@gmail.com>
2011-07-14 11:27:23 -0400

    response from snp now in its own function.

--- example.py
+++ example.py
@@ -1,18 +1,21 @@
 import pySNP    
 
 sig = 'SIG'
-token = ''
+uid = 'UID'
+password = ''
 title = 'TITLE'
 text = 'TEXT'
-icon = ''
-timeout = ''
-priority = ''
-uid = ''
-password = ''
+icon = 'ICON'
+cid = 'CID'
+timeout = '10'
+priority = 'PRIORITY'
+name = 'NAME'
+enabled = 'ENABLED'
 
-pySNP.snRegister(sig, token, title, icon, password)
-pySNP.snNotify(sig, token, title, text, icon, timeout, priority, uid, password)
+pySNP.snRegister(sig, password, title, icon)
+pySNP.snNotify(sig, uid, password, title, text, icon, cid, timeout, priority)
+pySNP.snAddClass(sig, password, title, text, icon, cid, name, enabled)
 pySNP.snVersion()
-pySNP.snUnregister(sig, token, password)
+pySNP.snUnregister(sig, password)
 
-#x = raw_input('Hit ANY key to exit')
+x = raw_input('Hit ANY key to exit')
--- pySNP.py
+++ pySNP.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-__author__ = "Shawn McTear"
-
 import socket
+
+__author__ = 'Shawn McTear'
+__version__ = '0.0.1'
 
 ip = '127.0.0.1'
 port = 9887
@@ -10,63 +11,86 @@ port = 9887
 def request(snp):
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.connect((ip, port))
-    sock.send(snp + '\r')
-    print sock.recv(1024) + snp + '\n'
+    sock.send(snp + '\r\n')
+    response(sock, snp)
     sock.close()
 
-def process(snp, req, data, value):
-    error = False
+def response(sock, snp):
+    resp = sock.recv(1024).rstrip('\r\n')
+    #TODO: check resp for snp error codes, then display error
+    print resp
+    print snp
+    print ''
+
+def process(snp, data, req, value):
+    err = error(False, '0', '')
     param = ''
 
     # Checks if data is required and if values are empty
-    for r, d, v in zip(req, data, value):
+    for d, r, v in zip(data, req, value):
         if r is True and len(v) > 0:
-            param = param + d + v
+            param = param + '&' + d + v
         elif r is True and len(v) <= 0:
-            param = param + ''
-            error = True
-            print 'Error: "%s" is missing.' % d
+            err = error(True, '109', d)
         elif r is False and len(v) > 0:
-            param = param + d + v
+            param = param + '&' + d + v
         elif r is False and len(v) <= 0:
-            param = param + ''
+             param = param
         else:
-            print 'Error: Unknown'
+            error(True, '', '')
 
-    # Checks if there where any errors before sending
-    if error is False:
+    # Checks for errors before sending
+    if err == False:
+        param = param.replace('&', '?', 1)
         snp = snp + param
     request(snp)
 
-def snRegister(sig, token, title, icon, password):
+def error(status, code, obj):
+    if status is False and code is '0':
+        return False
+    elif status is True and code is '109':
+        print 'Error: "%s" is missing.' % obj
+        return True
+    else:
+        print 'Error: Unknown'
+        return False
+
+def snRegister(sig, password, title, icon):
     print 'Snarl: Register'
     snp = 'snp://register'
-    req = [True, False, True, False, False]
-    data = ['?app-sig=', '&token=', '&title=', '&icon=', '&password=']
-    value = [sig, token, title, icon, password]
-    process(snp, req, data, value)
+    data = ['app-sig=', 'password=', 'title=', 'icon=']
+    req = [True, False, True, False]
+    value = [sig, password, title, icon]
+    process(snp, data, req, value)
 
-def snNotify(sig, token, title, text, icon, timeout, priority, uid, password):
+def snNotify(sig, uid, password, title, text, icon, cid, timeout, priority):
     print 'Snarl: Notify'
     snp = 'snp://notify'
-    req = [True, False, True, True, False, False, False, False, False]
-    data = ['?app-sig=', '&token=','&title=', '&text=', '&icon=', '&timeout=',
-            '&priority=', '&uid=', '&password=']
-    value = [sig, token, title, text, icon, timeout, priority, uid, password]
-    process(snp, req, data, value)
+    data = ['app-sig=', 'uid=', 'password=', 'title=', 'text=', 'icon=', 'id=', 'timeout=', 'priority=']
+    req = [True, False, False, True, True, False, False, False, False]
+    value = [sig, uid, password, title, text, icon, cid, timeout, priority]
+    process(snp, data, req, value)
+
+def snAddClass(sig, password, title, text, icon, cid, name, enabled):
+    print 'Snarl: AddClass'
+    snp = 'snp://addclass'
+    data = ['app-sig=', 'password=', 'title=', 'text=', 'icon=', 'id=', 'name=', 'enabled=']
+    req = [True, False, False, False, False, True, True, False]
+    value = [sig, password, title, text, icon, cid, name, enabled]
+    process(snp, data, req, value)
 
 def snVersion():
     print 'Snarl: Version'
     snp = 'snp://version'
-    req = []
     data = []
+    req = []
     value = []
-    process(snp, req, data, value)
+    process(snp, data, req, value)
 
-def snUnregister(sig, token, password):
+def snUnregister(sig, password):
     print 'Snarl: Unregister'
     snp = 'snp://unregister'
-    req = [True, False, False]
-    data = ['?app-sig=', '&token=', '&password=']
-    value = [sig, token, password]
-    process(snp, req, data, value)
+    data = ['app-sig=', 'password=']
+    req = [True, False]
+    value = [sig, password]
+    process(snp, data, req, value)
