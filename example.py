3147bbc4b8ca94d45c9ed83fda2ae5266177dd92
Shawn <syst3mfailur3@gmail.com>
2011-07-17 18:27:56 -0400

    Changed how parameters are added, less python errors.

--- example.py
+++ example.py
@@ -1,21 +1,10 @@
 import pySNP    
 
-sig = 'SIG'
-uid = 'UID'
-password = 'PASSWORD'
-title = 'TITLE'
-text = 'TEXT'
-icon = 'ICON'
-cid = 'CID'
-timeout = '10'
-priority = 'PRIORITY'
-name = 'NAME'
-enabled = 'ENABLED'
-
-pySNP.snRegister(sig, password, title, icon)
-pySNP.snNotify(sig, password, title, text, icon, cid, uid, timeout, priority)
-pySNP.snAddClass(sig, password, title, text, icon, cid, name, enabled)
+pySNP.snRegister('SIG', 'TITLE', password='testPASS', icon='testICON')
+pySNP.snNotify('SIG', 'TITLE', 'TEXT', password='testPASS', icon='testICON')
+pySNP.snAddClass('SIG', 'CID', 'CNAME', password='testPASS')
 pySNP.snVersion()
-pySNP.snUnregister(sig, password)
+pySNP.snUnregister('SIG', password='testPASS')
+
 
 #x = raw_input('Hit ANY key to exit')
--- pySNP.py
+++ pySNP.py
@@ -3,7 +3,7 @@
 import socket
 
 __author__ = 'Shawn McTear'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 ip = '127.0.0.1'
 port = 9887
@@ -26,23 +26,29 @@ def _response(recv, errors, snp):
     _error(2, None, errors, None)
     print snp
 
-def _process(action, data, value, req):
+def _process(action, data, args):
     errors = _error()
     snp = 'snp://' + action
     param = ''
 
-    #Checks if data is required and if values are empty
-    for d, v, r in zip(data, value, req):
-        if r is True and v:
-            param = param + '&' + d + v
-        elif r is True and not v:
-            errors = _error(1, 'missing', errors, d)
-        elif r is False and v:
-            param = param + '&' + d + v
+    # Fills data with info from args if needed
+    for d in data:
+        if d in args:
+            x = data[d]
+            x[2] = args[d]
+
+    # Checks if data is required and if values are empty
+    for key, value in sorted(data.items(), key=lambda x: x[1]):
+        if value[1] is True and value[2]:
+            param = param + '&' + key + '=' + value[2]
+        elif value[1] is True and not value[2]:
+            errors = _error(1, 'missing', errors, key)
+        elif value[1] is False and value[2]:
+            param = param + '&' + key + '=' + value[2]
         else:
             pass
 
-    #Checks for errors before sending
+    # Checks for errors before sending
     if not errors:
         param = param.replace('&', '?', 1)
         snp = snp + param
@@ -66,44 +72,51 @@ def _error(mode=0, issue=None, errors=None, obj=None):
         pass
     return errors
 
-def snRegister(sig, password, title, icon):
+def snRegister(sig='', title='', **args):
     print 'Snarl: Register'
     action = 'register'
-    data = ['app-sig=', 'password=', 'title=', 'icon=']
-    value = [sig, password, title, icon]
-    req = [True, False, True, False]
-    _process(action, data, value, req)
+    data = {'app-sig': [1, True, sig],
+            'password': [2, False, ''],
+            'title': [3, True, title],
+            'icon': [4, False, '']}
+    _process(action, data, args)
 
-def snNotify(sig, password, title, text, icon, cid, uid, timeout, priority):
-    print 'Snarl: Notify'
+def snNotify(sig='', title='', text='', **args):
+    print 'Snarl: Register'
     action = 'notify'
-    data = ['app-sig=', 'password=', 'title=', 'text=', 'icon=', 'id=',
-            'uid=', 'timeout=', 'priority=']
-    value = [sig, password, title, text, icon, cid, uid, timeout, priority]
-    req = [True, False, True, True, False, False, False, False, False]
-    _process(action, data, value, req)
+    data = {'app-sig': [1, True, sig],
+            'password': [2, False, ''],
+            'title': [3, True, title],
+            'text': [4, True, text],
+            'icon': [5, False, ''],
+            'id': [6, False, ''],
+            'uid': [7, False, ''],
+            'timeout': [8, False, ''],
+            'priority': [9, False, '']}
+    _process(action, data, args)
 
-def snAddClass(sig, password, title, text, icon, cid, name, enabled):
+def snAddClass(sig='', cid='', cname='', **args):
     print 'Snarl: AddClass'
     action = 'addclass'
-    data = ['app-sig=', 'password=', 'title=', 'text=', 'icon=', 'id=',
-            'name=', 'enabled=']
-    value = [sig, password, title, text, icon, cid, name, enabled]
-    req = [True, False, False, False, False, True, True, False]
-    _process(action, data, value, req)
+    data = {'app-sig': [1, True, sig],
+            'password': [2, False, ''],
+            'title': [3, False, ''],
+            'text': [4, False, ''],
+            'icon': [5, False, ''],
+            'id': [6, True, cid],
+            'name': [7, True, cname],
+            'enabled': [8, False, '']}
+    _process(action, data, args)
 
-def snVersion():
+def snVersion(**args):
     print 'Snarl: Version'
     action = 'version'
-    data = []
-    value = []
-    req = []
-    _process(action, data, value, req)
+    data = {}
+    _process(action, data, args)
 
-def snUnregister(sig, password):
+def snUnregister(sig='', **args):
     print 'Snarl: Unregister'
     action = 'unregister'
-    data = ['app-sig=', 'password=']
-    value = [sig, password]
-    req = [True, False]
-    _process(action, data, value, req)
+    data = {'app-sig': [1, True, sig],
+            'password': [2, False, '']}
+    _process(action, data, args)
